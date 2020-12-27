274c42f543ea776b912b3f5bff1d1fa0a9a9fa09
Tsegen <Paul.Tsegen@ng.kpmg.com>
2020-12-27 19:08:38 +0100

    practice project

---
+++ main.js
@@ -0,0 +1,21 @@
+// Selectors
+const calcDisplay = document.querySelector('.calc-display');
+const numBtnList = document.querySelectorAll('.btn-numList');
+const numBtns = document.querySelector('.num-btn')
+// operator keys selectors
+const delBtn = document.querySelector('#delete-btn');
+const clrBtn = document.querySelector('#clear-btn');
+const addBtn = document.querySelector('#add-btn');
+const minusBtn = document.querySelector('#minus-btn');
+const divideBtn = document.querySelector('#divide-btn');
+const multipyBtn = document.querySelector('#multipy-btn');
+const equalsBtn = document.querySelector('#equals2-btn')
+
+// functions
+const test = () => {
+    console.log('testing')
+}
+
+// EventListners 
+// numBtnList.addEventListener('click',retrieve);
+delBtn.addEventListener('click',test);
